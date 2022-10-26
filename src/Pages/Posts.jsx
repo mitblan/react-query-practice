b54f24510984c818c8d09f615e5ffde39f5a2c3d
mitblan <mitblan@gmail.com>
2022-10-26 16:05:17 -0400

    Basic pagination working

--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -6,22 +6,24 @@ import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import PostItem from '../Component/PostItem'
 
-async function fetchPosts() {
-	const response = await fetch(
-		'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=1'
-	)
-
-	return response.json()
-}
-
 function Posts() {
 	const [currentPage, setCurrentPage] = useState(1)
+
+	async function fetchPosts() {
+		const response = await fetch(
+			`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${currentPage}`
+		)
+
+		return response.json()
+	}
 
 	const {
 		isLoading,
 		error,
 		data: posts,
-	} = useQuery(['posts'], fetchPosts, { staleTime: 300000 })
+	} = useQuery(['posts', currentPage], () => fetchPosts(currentPage), {
+		staleTime: 300000,
+	})
 
 	if (isLoading) {
 		return <h1>Loading...</h1>
@@ -40,6 +42,11 @@ function Posts() {
 			{posts.map((post) => (
 				<PostItem key={post.id} post={post} />
 			))}
+			<div className='pagination-wrapper'>
+				<button onClick={() => setCurrentPage(currentPage - 1)}>Prev</button>
+				<span>{currentPage}</span>
+				<button onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+			</div>
 		</>
 	)
 }
